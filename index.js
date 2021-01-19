834150c6b0118ed604a18fb084d551a164168683
CJ McCaskill <cjmccaskill@gmail.com>
2021-01-18 20:14:32 -0600

    Set up as NPM module

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-// Reverse string
-function reverse(string) {
-  return Array.from(string).reverse().join("");
-}
+module.exports = Phrase;
 
 // Adds`reverse` to all strings
 String.prototype.reverse = function () {
   return Array.from(this).reverse().join("");
 };
-
-// // Returns true if the string is empty or consists solely of whitespace.
-// String.prototype.blank = function () {
-//   return !!this.match(/^\s*$/g);
-// };
-// // Add 'last' method to all arrays
-// Array.prototype.last = function () {
-//   return this[this.length - 1];
-// };
 
 // Define a Phrase object
 function Phrase(content) {
@@ -36,18 +24,3 @@ function Phrase(content) {
     return this.processedContent() === this.processedContent().reverse();
   };
 }
-
-// DEMO on Translating Phrases
-
-// // Define a TranslatedPhrase object
-// function TranslatedPhrase(content, translation) {
-//   this.content = content;
-//   this.translation = translation;
-
-//   // Returns translation processed for palindrome testing
-//   this.processedContent = function processedContent() {
-//     return this.processor(this.translation);
-//   };
-// }
-
-// TranslatedPhrase.prototype = new Phrase();
